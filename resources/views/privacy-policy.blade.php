afca6d91bfbde0312ebd77730aa524ce144b23a2
Edivaldo Jorge <32965474+jorgeedvaldo@users.noreply.github.com>
2023-07-14 13:19:18 +0000

    create the terms of service and privacy policy

---
+++ resources/views/privacy-policy.blade.php
@@ -0,0 +1,89 @@
+@extends('template.app')
+@section('title', 'Privacy and Policy | ')
+@section('content')
+<section class="bg-blue-500 py-4">
+    <div class="container mx-auto px-4">
+        <h1 class="text-white text-2xl font-bold">Privacy Policy</h1>
+    </div>
+</section>
+<section>
+    <h2 class="text-2xl font-bold mb-4">Personal identification information</h2>
+    <p class="mb-6">We may collect personal identification information from Users in a variety of ways, including, but
+        not limited to, when Users visit our site, fill out a form, and in connection with other activities, services,
+        features or resources we make available on our Site. Users may visit our Site anonymously. We will collect
+        personal identification information from Users only if they voluntarily submit such information to us. Users can
+        always refuse to supply personally identification information, except that it may prevent them from engaging in
+        certain Site related activities.</p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">Non-personal identification information</h2>
+    <p class="mb-6">We may collect non-personal identification information about Users whenever they interact with our
+        Site. Non-personal identification information may include the browser name, the type of computer and technical
+        information about Users means of connection to our Site, such as the operating system and the Internet service
+        providers utilized and other similar information.</p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">Web browser cookies</h2>
+    <p class="mb-6">We may collect non-personal identification information about Users whenever they interact with our
+        Site. Non-personal identification information may include the browser name, the type of computer and technical
+        information about Users means of connection to our Site, such as the operating system and the Internet service
+        providers utilized and other similar information.</p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">How we use collected information</h2>
+    <p class="mb-6">DlThreads.net does not collect or store your data, period.</p>
+    <p class="mb-6">DlThreads.net does not collect or store your data, period.</p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">Advertising</h2>
+    <p class="mb-6">Ads appearing on our site may be delivered to Users by advertising partners, who may set cookies.
+        These cookies allow the ad server to recognize your computer each time they send you an online advertisement to
+        compile non personal identification information about you or others who use your computer. This information
+        allows ad networks to, among other things, deliver targeted advertisements that they believe will be of most
+        interest to you. This privacy policy does not cover the use of cookies by any advertisers.</p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">Google Adsense</h2>
+    <p class="mb-6">Some of the ads may be served by Google. Google's use of the DART cookie enables it to serve ads to
+        Users based on their visit to our Site and other sites on the Internet. DART uses "non personally identifiable
+        information" and does NOT track personal information about you, such as your name, email address, physical
+        address, etc. You may opt out of the use of the DART cookie by visiting the Google ad and content network
+        privacy policy at <a href="https://policies.google.com/technologies/ads"
+            class="text-blue-500 underline">https://policies.google.com/technologies/ads</a></p>
+    <p class="mb-6">Some of the ads may be served by Google. Google's use of the DART cookie enables it to serve ads to
+        Users based on their visit to our Site and other sites on the Internet. DART uses "non personally identifiable
+        information" and does NOT track personal information about you, such as your name, email address, physical
+        address, etc. You may opt out of the use of the DART cookie by visiting the Google ad and content network
+        privacy policy at <a href="https://policies.google.com/technologies/ads"
+            class="text-blue-500 underline">https://policies.google.com/technologies/ads</a></p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">Changes to this privacy policy</h2>
+    <p class="mb-6">DlThreads.net has the discretion to update this privacy policy at any time. When we do, we will post
+        a notification on the main page of our Site, revise the updated date at the bottom of this page. We encourage
+        Users to frequently check this page for any changes to stay informed about how we are helping to protect the
+        personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy
+        policy periodically and become aware of modifications.</p>
+    <p class="mb-6">DlThreads.net has the discretion to update this privacy policy at any time. When we do, we will post
+        a notification on the main page of our Site, revise the updated date at the bottom of this page. We encourage
+        Users to frequently check this page for any changes to stay informed about how we are helping to protect the
+        personal information we collect. You acknowledge and agree that it is your responsibility to review this privacy
+        policy periodically and become aware of modifications.</p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">Your acceptance of these terms</h2>
+    <p class="mb-6">By using this Site, you signify your acceptance of this policy. If you do not agree to this policy,
+        please do not use our Site. Your continued use of the Site following the posting of changes to this policy will
+        be deemed your acceptance of those changes.</p>
+    <p class="mb-6">By using this Site, you signify your acceptance of this policy. If you do not agree to this policy,
+        please do not use our Site. Your continued use of the Site following the posting of changes to this policy will
+        be deemed your acceptance of those changes.</p>
+</section>
+@endsection
---
+++ resources/views/terms-of-service.blade.php
@@ -0,0 +1,72 @@
+@extends('template.app')
+@section('title', 'Terms of Service | ')
+@section('content')
+<section class="bg-blue-500 py-4">
+    <div class="container mx-auto px-4">
+        <h1 class="text-white text-2xl font-bold">Terms of Service</h1>
+    </div>
+</section>
+<section>
+    <h2 class="text-2xl font-bold mb-4">1. Acceptance of Terms</h2>
+    <p class="mb-6">By accessing or using the DlThreads.net website and services (the "Service"), you agree to be bound
+        by these Terms of Service ("Terms"). If you do not agree to these Terms, you may not access or use the Service.
+    </p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">2. Description of Service</h2>
+    <p class="mb-6">DlThreads.net provides a platform for users to access and share threads. The Service may include
+        advertisements, which are necessary for DlThreads.net to continue providing the Service.</p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">3. User Conduct</h2>
+    <p class="mb-6">You agree to use the Service in compliance with all applicable laws and regulations. You are solely
+        responsible for your conduct while using the Service, and you must not:
+    <ul class="list-disc ml-8 mb-6">
+        <li>Violate any laws or regulations;</li>
+        <li>Post, upload, or transmit any content that is illegal, harmful, defamatory, abusive, harassing, invasive of
+            privacy, or otherwise objectionable;</li>
+        <li>Impersonate any person or entity or falsely state or otherwise misrepresent your affiliation with a person
+            or entity;</li>
+        <li>Interfere with or disrupt the integrity or performance of the Service;</li>
+        <li>Attempt to gain unauthorized access to the Service or its related systems or networks;</li>
+        <li>Engage in any other conduct that restricts or inhibits anyone's use or enjoyment of the Service.</li>
+    </ul>
+    </p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">4. Intellectual Property Rights</h2>
+    <p class="mb-6">The Service and its original content, features, and functionality are owned by DlThreads.net and are
+        protected by international copyright, trademark, patent, trade secret, and other intellectual property or
+        proprietary rights laws.</p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">5. Limitation of Liability</h2>
+    <p class="mb-6">DlThreads.net and its directors, employees, partners, agents, suppliers, or affiliates shall not be
+        liable for any indirect, incidental, special, consequential, or punitive damages, including without limitation,
+        loss of profits, data, use, goodwill, or other intangible losses, resulting from your access to or use of or
+        inability to access or use the Service.</p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">6. Governing Law</h2>
+    <p class="mb-6">These Terms shall be governed by and construed in accordance with the laws of [your jurisdiction].
+        Any disputes arising under or in connection with these Terms shall be subject to the exclusive jurisdiction of
+        the courts located in [your jurisdiction].</p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">7. Changes to Terms</h2>
+    <p class="mb-6">DlThreads.net reserves the right, at its sole discretion, to modify or replace these Terms at any
+        time. If a revision is material, we will provide at least 30 days' notice prior to any new terms taking effect.
+        What constitutes a material change will be determined at our sole discretion.</p>
+</section>
+
+<section>
+    <h2 class="text-2xl font-bold mb-4">8. Contact Us</h2>
+    <p class="mb-6">If you have any questions about these Terms, please contact us at [contact email address].</p>
+</section>
+@endsection
