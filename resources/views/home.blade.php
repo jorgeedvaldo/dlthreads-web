1f5f7d5e9d7ee9f35a9768456ee36d70c68be6aa
Edivaldo Jorge <32965474+jorgeedvaldo@users.noreply.github.com>
2023-07-17 10:55:01 +0000

    token

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
         <h2 class="text-3xl font-bold text-white mb-4">Threads Video Download</h2>
         <p class="text-gray-100 mb-8">Download Video Threads, Photo online</p>
         <form method="POST" action="{{ route('home') }}" class="flex items-center">
+        @csrf
             <div class="relative w-full">
                 <input type="text" placeholder="Enter Threads post URL" name="url"
                     class="w-full rounded-l px-4 py-2 outline-none focus:ring-2 focus:ring-blue-500 url-input" value="">
